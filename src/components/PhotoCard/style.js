4f35d3b198fde85a4192aff08f112f33d63fbc34
Frcomparan <frcomparan@gmail.com>
2022-07-01 09:14:06 -0500

    Create PhotoCard and ListOfPhotoCard components

---
+++ src/components/PhotoCard/style.js
@@ -0,0 +1,30 @@
+import styled from "styled-components"
+
+export const ImgWrapper = styled.figure`
+  border-radius: 10px;
+  margin: 0;
+  display: block;
+  overflow: hidden;
+  height: 0;
+  padding: 56.25% 0 0 0;
+  position: relative;
+  width: 100%;
+`
+
+export const Img = styled.img`
+  box-shadow: 0 10px 14px rgba(0, 0, 0, .2);
+  height: 100%;
+  object-fit: cover;
+  position: absolute;
+  top: 0;
+  width: 100%;
+`
+
+export const Button = styled.button`
+  padding-top: 8px;
+  display: flex;
+  align-items: center;
+  & svg {
+    margin-right: 4px;
+  }
+`
