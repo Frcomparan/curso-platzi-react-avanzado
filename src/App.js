230e3b1ef069a432401a26173375ec0652bfebeb
Frcomparan <frcomparan@gmail.com>
2022-07-01 08:45:36 -0500

    Create Category and ListOfCategory componentsx

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { ListOfCategories } from "./components/ListOfCategories/index"
+import { GlobalStyle } from "./GlobalStyles"
+
+export const App = () => (
+  <div>
+    <GlobalStyle></GlobalStyle>
+    <ListOfCategories />
+  </div>
+)
