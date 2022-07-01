94c2532af2e0a216f35238082846a3de4698fda7
Frcomparan <frcomparan@gmail.com>
2022-07-01 10:32:20 -0500

    Add floating ListOfCategories

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './style.js'
-import { categories } from '../../../api/db.json'
 
-export const ListOfCategories = () =>  (
-  <List>
-    {
-      categories.map(category => <Item key={category.id}><Category {...category} /></Item> )
+export const ListOfCategories = () =>  {
+  const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const response = await window.fetch('https://petgram-server-frc.vercel.app/categories')
+      const data = await response.json()
+      setCategories(data)
     }
-  </List>
-)
+    fetchCategories()
+  },[])
+
+  useEffect(() => {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed != newShowFixed && setShowFixed(newShowFixed)
+    }
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed && 'fixed'}>
+      {
+        categories.map(category => <Item key={category.id}><Category {...category} /></Item> )
+      }
+    </List>
+  )
+
+  return (
+    <React.Fragment>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </React.Fragment>
+  )
+}
