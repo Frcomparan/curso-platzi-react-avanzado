537548443084b29cb80a5e88960adce3a3f4240a
Frcomparan <frcomparan@gmail.com>
2022-07-03 16:21:55 -0500

    First use of Apolo

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,10 +1,37 @@
 import React from "react"
 import { PhotoCard } from "../PhotoCard"
 
-export const ListOfPhotoCards = () => (
-  <ul>
-    {
-      [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(id => <PhotoCard key={id} id={id} />)
+import { graphql } from "graphql"
+import { useQuery,gql } from "@apollo/client"
+
+const withPhotos = gql`
+  query getPhotos{
+    photos {
+      id
+      categoryId
+      src
+      likes
+      userId
+      liked
     }
-  </ul>
-)
+  }
+`
+
+export const ListOfPhotoCards = () => {
+  const { loading, error, data } = useQuery(withPhotos)
+
+  if (error) {
+    return <h2>Internal Server Error</h2>
+  }
+  if (loading) {
+    return <h2>Loading...</h2>
+  }
+
+  return (
+    <ul>
+      {data.photos.map((photo) => (
+        <PhotoCard key={photo.id} {...photo} />
+      ))}
+    </ul>
+  )
+}
