181332688a06d7434faa7c8c6f30eb25149ddfdb
Frcomparan <frcomparan@gmail.com>
2022-07-03 18:31:35 -0500

    Add all routes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import React from "react"
 import { GlobalStyle } from "./styles/GlobalStyles"
 import { Logo } from "./components/Logo"
-import { PhotoCardWithQuery } from "./container/PhotoCardWithQuery"
 import { Home } from "./pages/Home"
 import { Route, Routes, BrowserRouter } from "react-router-dom"
+import { Detail } from "./pages/Detail"
+import { NavBar } from "./components/NavBar"
+import { User } from "./pages/User"
+import { Favs } from "./pages/Favs"
+import { NotRegisteredUser } from "./pages/NotRegisteredUser" 
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
 
 export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search)
-  const detailId = urlParams.get('detail')
 
   return (
     <div>
     <BrowserRouter>
       <GlobalStyle></GlobalStyle>
       <Logo />
-      {
-        detailId 
-        ? <PhotoCardWithQuery id={detailId}/>
-        : 
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/pet/:id' element={<Home />} />
-          </Routes>
-      }
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/pet/:id' element={<Home />} />
+        <Route path='/detail/:detailId' element={<Detail />} />
+      </Routes>
+        <UserLogged>
+          {
+            ({ isAuth }) => isAuth 
+            ? <Routes>
+                <Route path='/favs' element={<Favs />} />
+                <Route path='/user' element={<User />} />
+              </Routes>
+            : <Routes>
+                <Route path='/user' element={<NotRegisteredUser />} />
+                <Route path='/favs' element={<NotRegisteredUser />} /> 
+              </Routes>
+          }
+        </UserLogged>
+      <NavBar></NavBar>
     </BrowserRouter>
     </div>
   )
---
+++ src/pages/NotRegisteredUser.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export const NotRegisteredUser = () => {
+
+  return (
+    <h1>Not Registered</h1>
+  )
+}
